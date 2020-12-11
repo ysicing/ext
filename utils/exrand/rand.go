349cd9c6d5a7a05b6d6ec5b110d728d402f09dfb
ysicing <i@ysicing.me>
2020-12-11 15:34:43 +0800

    feat: add rand
    Signed-off-by: ysicing <i@ysicing.me>

--- utils/exrand/rand.go
+++ utils/exrand/rand.go
@@ -4,6 +4,7 @@
 package exrand
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -42,4 +43,27 @@ func StringRand(len int) string {
 		bytes[i] = byte(b)
 	}
 	return string(bytes)
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+var digits = []rune("0123456789")
+
+const size = 62
+
+func RandLetters(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(size)]
+	}
+
+	return fmt.Sprintf("%s", string(b))
+}
+
+func RandDigits(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = digits[rand.Intn(10)]
+	}
+
+	return fmt.Sprintf("%s", string(b))
 }
