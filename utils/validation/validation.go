3e8bee966dd3753fb5e0445b21ef31b06786e5ad
ysicing <i@ysicing.me>
2020-12-11 15:43:59 +0800

    feat: update code
    Signed-off-by: ysicing <i@ysicing.me>

--- utils/validation/validation.go
+++ utils/validation/validation.go
@@ -2,3 +2,38 @@
 // Copyright (c) 2020 ysicing <i@ysicing.me>
 
 package validation
+
+import "strings"
+
+//Dangerous 是否危险
+func Dangerous(s string) bool {
+	if strings.Contains(s, "<") {
+		return true
+	}
+
+	if strings.Contains(s, ">") {
+		return true
+	}
+
+	if strings.Contains(s, "&") {
+		return true
+	}
+
+	if strings.Contains(s, "'") {
+		return true
+	}
+
+	if strings.Contains(s, "\"") {
+		return true
+	}
+
+	if strings.Contains(s, "file://") {
+		return true
+	}
+
+	if strings.Contains(s, "../") {
+		return true
+	}
+
+	return false
+}
